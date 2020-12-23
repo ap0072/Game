6f59ee2039de5e0951da8ff39a927a0c39963638
prasanth2020501099 <chaluvadikumar97@msitprogram.net>
2020-12-23 21:00:20 +0530

    stage 2 of commit with all src files

---
+++ def/src/sample_1/Iphone.java
@@ -0,0 +1,8 @@
+package sample_1;
+
+public class Iphone {
+public Iphone() {
+	System.out.println("I brought a new iphone ");
+
+}
+}
---
+++ def/src/sample_1/Phone.java
@@ -0,0 +1,14 @@
+package sample_1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Phone {
+
+	@Test
+	void test() {
+		
+	}
+
+}
